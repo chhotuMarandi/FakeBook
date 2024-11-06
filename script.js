4e99aebd5992574c7457ad6e7a19c28cd67f9612
Chhotu Marandi <cmarandi29@gmail.com>
2024-11-06 21:39:10 +0530

    add check valid email and input filled

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const password = document.querySelector('.password')
+const email = document.querySelector('.email')
 const passShow = document.querySelector('.pass-show')
 const passHide = document.querySelector('.pass-hide')
+const login = document.querySelector('.login')
+const error = document.querySelector('.error')
+const mailError = document.querySelector('.mail-error')
 
-//show password
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+//show password function
 
 function showPass() {
   if (passHide) {
@@ -12,7 +18,7 @@ function showPass() {
   }
 }
 
-//Hide password
+//Hide password function
 
 function hidePass() {
   if (passHide) {
@@ -22,7 +28,33 @@ function hidePass() {
   }
 }
 
+// submit function
+
+function submit() {
+  //check if email and pass are filled
+  if (!email.value || !password.value === null) {
+    error.style.display = 'block'
+    setTimeout(() => {
+      error.style.display = 'none'
+    }, 2000)
+  }
+
+  //check email is valid
+
+  if (email.value) {
+    if (!emailPattern.test(email.value)) {
+      mailError.style.display = 'block'
+
+      setTimeout(() => {
+        mailError.style.display = 'none'
+      }, 2000)
+    }
+  }
+}
+
 //Event Listener
+
+login.addEventListener('click', submit)
 
 password.addEventListener('input', () => {
   passHide.style.display = 'block'
